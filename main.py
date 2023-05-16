be559bf048d9099d126fd901673c466ac0218732
Evgenii Sergunin <mckrei@ya.ru>
2023-05-16 20:39:18 +0300

    flask wtf

--- main.py
+++ main.py
@@ -1,36 +1,70 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import Length, DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'SECRET_KEY'
 
-context = {'title': 'Заголовок',
-           'text': 'Текст'}
+news = [{'title': 'Удивительное событие в школе',
+         'text': 'Вчера в местной школе произошло удивительное событие - все '
+                 'ученики одновременно зевнули на уроке математики. '
+                 'Преподаватель был так поражен этим коллективным зевком, '
+                 'что решил отменить контрольную работу.'},
+        {'title': 'Случай в зоопарке',
+         'text': 'В зоопарке города произошел необычный случай - ленивец '
+                 'решил не лениться и взобрался на самое высокое дерево в '
+                 'своем вольере. Посетители зоопарка были поражены такой '
+                 'активностью и начали снимать ленивца на видео. В итоге он '
+                 'получил свой собственный канал на YouTube, где он размещает '
+                 'свои приключения.'},
+        {'title': 'Самый красивый пёс',
+         'text': 'Сегодня в парке прошел необычный конкурс - "Самый красивый '
+                 'пёс". Участники конкурса были так красивы, что судьи не '
+                 'могли выбрать победителя. В итоге, конкурс был объявлен '
+                 'ничейным, а участники получили награды за участие, '
+                 'в том числе - пакетики конфет и игрушки в виде косточек. '
+                 'Конкурс вызвал большой интерес у посетителей парка, '
+                 'и его решили повторить в более масштабном формате.'}]
+
+
+class NewsForm(FlaskForm):
+    title = StringField(
+        'Название',
+        validators=[DataRequired(message="Поле не должно быть пустым"),
+                    Length(max=255, message='Введите заголовок длиной до 255 символов')]
+    )
+    text = TextAreaField(
+        'Текст',
+        validators=[DataRequired(message="Поле не должно быть пустым")])
+    submit = SubmitField('Добавить')
 
 
 @app.route('/')
 def index():
-    class New:
-        def __init__(self, name, age):
-            self.name = name
-            self.age = age
-
-    new = New('Vasya', 20)
-    lst = [1, 2, 3, 4, 5, 6]
-    for i in lst:
-        print(i)
-    else:
-        print('else')
-
-    return render_template(
-        'index.html',
-        number=4,
-        hello='hello',
-        world='hello',
-        context=context,
-        new=new,
-        lst=[1, 2, 3, 4, 5, 6]
-    )
+    return render_template('index.html',
+                           news=news)
 
 
+@app.route('/news_detail/<int:id>')
+def news_detail(id):
+    title = news[id]['title']
+    text = news[id]['text']
+    return render_template('news_detail.html',
+                           title=title,
+                           text=text)
+
+
+@app.route('/add_news', methods=['GET', 'POST'])
+def add_news():
+    form = NewsForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        text = form.text.data
+        news.append({'title': title, 'text': text})
+        return redirect(url_for('index'))
+    return render_template('add_news.html',
+                           form=form)
 
 
 if __name__ == '__main__':
