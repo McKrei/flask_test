2c7cd6cf5638aad36bd70ff2597bc4cd94dbd024
Evgenii Sergunin <mckrei@ya.ru>
2023-05-16 21:29:58 +0300

    add

--- main.py
+++ main.py
@@ -1,32 +1,45 @@
-from flask import Flask, render_template, redirect, url_for
+from datetime import datetime
 
+from flask import Flask, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import Length, DataRequired
+from wtforms import StringField, TextAreaField, SelectField, SubmitField
+from wtforms.validators import DataRequired, Length, Optional
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'SECRET_KEY'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
-news = [{'title': 'Удивительное событие в школе',
-         'text': 'Вчера в местной школе произошло удивительное событие - все '
-                 'ученики одновременно зевнули на уроке математики. '
-                 'Преподаватель был так поражен этим коллективным зевком, '
-                 'что решил отменить контрольную работу.'},
-        {'title': 'Случай в зоопарке',
-         'text': 'В зоопарке города произошел необычный случай - ленивец '
-                 'решил не лениться и взобрался на самое высокое дерево в '
-                 'своем вольере. Посетители зоопарка были поражены такой '
-                 'активностью и начали снимать ленивца на видео. В итоге он '
-                 'получил свой собственный канал на YouTube, где он размещает '
-                 'свои приключения.'},
-        {'title': 'Самый красивый пёс',
-         'text': 'Сегодня в парке прошел необычный конкурс - "Самый красивый '
-                 'пёс". Участники конкурса были так красивы, что судьи не '
-                 'могли выбрать победителя. В итоге, конкурс был объявлен '
-                 'ничейным, а участники получили награды за участие, '
-                 'в том числе - пакетики конфет и игрушки в виде косточек. '
-                 'Конкурс вызвал большой интерес у посетителей парка, '
-                 'и его решили повторить в более масштабном формате.'}]
+db = SQLAlchemy(app)
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    news = db.relationship('News', back_populates='category')
+
+    def __repr__(self):
+        return f'Category {self.id}: ({self.title})'
+
+
+class News(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), unique=True, nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    created_date = db.Column(db.DateTime, default=datetime.utcnow)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=True)
+    category = db.relationship('Category', back_populates='news')
+
+    def __repr__(self):
+        return f'News {self.id}: ({self.title[:20]}...)'
+
+
+db.create_all()
+
+
+def get_categories():
+    categories = Category.query.all()
+    return [(category.id, category.title) for category in categories]
 
 
 class NewsForm(FlaskForm):
@@ -38,34 +51,55 @@ class NewsForm(FlaskForm):
     text = TextAreaField(
         'Текст',
         validators=[DataRequired(message="Поле не должно быть пустым")])
+    category = SelectField('Категория', choices=get_categories, validators=[Optional()])
     submit = SubmitField('Добавить')
 
 
 @app.route('/')
 def index():
+    news_list = News.query.all()
+    categories = Category.query.all()
     return render_template('index.html',
-                           news=news)
+                           news=news_list[::-1],
+                           categories=categories)
 
 
 @app.route('/news_detail/<int:id>')
 def news_detail(id):
-    title = news[id]['title']
-    text = news[id]['text']
+    news = News.query.get(id)
+    categories = Category.query.all()
     return render_template('news_detail.html',
-                           title=title,
-                           text=text)
+                           news=news,
+                           categories=categories)
+
+
+@app.route('/category/<int:id>')
+def news_in_category(id):
+    category = Category.query.get(id)
+    news = category.news
+    category_name = category.title
+    categories = Category.query.all()
+    return render_template('category.html',
+                           news=news,
+                           category_name=category_name,
+                           categories=categories)
 
 
 @app.route('/add_news', methods=['GET', 'POST'])
 def add_news():
     form = NewsForm()
+    categories = Category.query.all()
     if form.validate_on_submit():
-        title = form.title.data
-        text = form.text.data
-        news.append({'title': title, 'text': text})
-        return redirect(url_for('index'))
+        news = News()
+        news.title = form.title.data
+        news.text = form.text.data
+        news.category_id = form.category.data
+        db.session.add(news)
+        db.session.commit()
+        return redirect(url_for('news_detail', id=news.id))
     return render_template('add_news.html',
-                           form=form)
+                           form=form,
+                           categories=categories)
 
 
 if __name__ == '__main__':
