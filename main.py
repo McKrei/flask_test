933386e5e36508c93c51e2791f9343447e1d7411
Evgenii Sergunin <mckrei@ya.ru>
2023-05-16 21:16:03 +0300

    add

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, url_for
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import Length, DataRequired
